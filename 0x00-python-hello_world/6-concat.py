a50dec051d1eedc726baa72047fba89b8f554059
John Agbo <Jonchukwuma04@gmail.com>
2022-06-27 18:11:03 -0700

    This Python script executes toconcatenate strings together

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 first_str = "Holberton"
 second_str = "School"
-concat_str = first_str + " " + second_str
-print(f"Welcome to {concat_str}!")
+first_str = first_str + " " + second_str
+print(f"Welcome to {first_str}!")
