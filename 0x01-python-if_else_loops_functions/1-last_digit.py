43357b1e47fecdc94d2476aa9fae44643bfada1a
John Agbo <Jonchukwuma04@gmail.com>
2022-06-28 15:57:01 -0700

    This code assigns a random number to a variable

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,21 +2,17 @@
 import random
 number = random.randint(-10000, 10000)
 
-if number < 0:
-    buf = number * -1
-else:
-    buf = number
+if number >= 0:
+    last_digit = number % 10
+elif number < 0:
+    last_digit = ((number * -1) % 10) * -1
 
-lastDigit = buf % 10
-if number < 0:
-    lastDigit = lastDigit * -1
+if last_digit > 5:
+    str = 'Last digit of {0} is {1} and is greater than 5'
+elif last_digit < 6 and last_digit != 0:
+    str = 'Last digit of {0} is {1} and is less than 6 and not 0'
+elif last_digit == 0:
+    str = 'Last digit of {0} is {1} and is 0'
 
-if lastDigit == 0:
-    print(f"Last digit of {number:d} is 0 and is 0")
+print(str.format(number, last_digit))
 
-elif lastDigit > 5:
-    print(f"Last digit of {number:d} is {lastDigit:d} and is greater than 5")
-
-elif lastDigit < 6:
-    print(f"Last digit of {number:d} is {lastDigit:d}\
-and is less than 6 and not 0")
