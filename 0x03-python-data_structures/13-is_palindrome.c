6d7dc444c6ddc8d2edff9ea0935592344ad57a3f
John Agbo <Jonchukwuma04@gmail.com>
2022-07-05 06:54:10 -0700

    This code executes to check if a singlylinked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,41 +1,74 @@
 #include "lists.h"
 
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
+
+/**
+ * reverse_listint - Reverses a singly-linked listint_t list
+ *
+ * @head: A pointer to the starting node of the list to reverse.
+ * Return: A pointer to the head of the reversed list.
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
 /**
  * is_palindrome - This function checks if a singly linked list
  * is a palindrome
  *
- * @head: specifies the start of the node
+ * @head: specifies the start of a the node
  * Return: Always 0. (Success)
  */
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head;
-	listint_t *temp = *head;
-	int head_val, tail_val;
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
 
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	head_val = (*head)->n;
 
-	while (current)
+	tmp = *head;
+	while (tmp)
 	{
-		if ((current->next)->next == NULL)
-		{
-			tail_val = (current->next)->n;
-			current->next = NULL;
-
-			if (head_val != tail_val)
-				return (0);
-			temp = temp->next;
-
-			if (!temp)
-				break;
-			head_val = temp->n;
-			current = temp;
-		}
-		else
-			current = current->next;
+		size++;
+		tmp = tmp->next;
 	}
-	return(1);
+
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
+
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
+
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
+	{
+		if (tmp->n != rev->n)
+			return (0);
+		tmp = tmp->next;
+		rev = rev->next;
+	}
+	reverse_listint(&mid);
+
+	return (1);
 }
