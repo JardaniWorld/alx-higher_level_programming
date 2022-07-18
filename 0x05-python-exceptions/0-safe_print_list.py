b54350b77a8296662056e0e886799191614ec5c4
John Agbo <Jonchukwuma04@gmail.com>
2022-07-18 07:09:07 -0700

    This code prints the elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    element_num = 0
+    for y in range(x):
+        try:
+            print('{:d}'.format(my_list[y]), end="")
+            element_num += 1
+        except IndexError:
+            break
+        print("")
+        return element_num
