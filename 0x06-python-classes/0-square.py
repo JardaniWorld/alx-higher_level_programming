5f63fd4a76d2a0bff8844e0510c64156daa42c08
John Agbo <Jonchukwuma04@gmail.com>
2022-07-19 16:37:01 -0700

    This code creates an empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-# 0-square.py
-"""Square Class
-
-This is an empty class
-
+"""Creates an empty class
 """
 
 class Square:
-    """A class with name Square."""
+    """Represents an empty square."""
     pass
