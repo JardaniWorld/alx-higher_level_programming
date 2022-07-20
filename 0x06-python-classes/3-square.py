3eb2d80f9349c1be95fa6addab452690bd2a2fae
John Agbo <Jonchukwuma04@gmail.com>
2022-07-19 17:20:16 -0700

    This code defines a Square class with a private attribute and raises an exception

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-# 3-square.py
-"""Define a class Square."""
+"""returns the current square area"""
 
 
 class Square:
-    """Represent a square."""
-
+    """defines a square and raises both TypeError and ValueError if
+    size is not int or < 0 respectively"""
     def __init__(self, size=0):
-        """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
 
-                                                                                    def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+    def area(self):
+        return self.__size ** 2
