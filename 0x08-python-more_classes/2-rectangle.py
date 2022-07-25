2d1bc979a56cd1a6ecf0236ace436c9516e42610
John Agbo <Jonchukwuma04@gmail.com>
2022-07-25 13:30:29 -0700

    This code defines a class: Rectangle with some specifications

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -45,11 +45,11 @@ class Rectangle:
 
     def area(self):
         """returns rectangle area"""
-        return self.__width * self.__height
+        return (self.__width * self.__height)
 
     def perimeter(self):
         """returns rectangle perimeter"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width + self.__height)
+        return ((self.__width * 2) + (self.__height * 2))
 
