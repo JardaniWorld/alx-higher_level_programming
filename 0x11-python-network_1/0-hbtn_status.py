7eda74834f53e8b204e182d60e577b5310c3beb4
JardaniWorld <Jonchukwuma04@gmail.com>
2022-10-21 14:28:34 -0700

    This script fetches a url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status."""
-from urllib.request import urlopen
+"""Fetches the URL: https://intranet.hbtn.io/status
+"""
 
-if __name__ == '__main__':
-    with urlopen('https://alx-intranet.hbtn.io/status') as res:
+from urllib.request import Request, urlopen
+
+
+if __name__ == "__main__":
+    req = Request('https://alx-intranet.hbtn.io/status')
+
+    with urlopen(req) as res:
         content = res.read()
-        print('Body reponse:')
-        print('\t- type:', type(content))
-        print('\t- content:', content)
-        print('\t- utf8 content:', content.decode('utf-8'))
+        utf8_content = content.decode('utf-8')
+
+        print('Body response:')
+        print('\t- type: {_type}'.format(_type=type(content)))
+        print('\t- content: {_content}'.format(_content=content))
+        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
